1a700c73392b73e1700919ee4d366bcd679ff19b
何金华 <hejh@irongbei.com>
2019-08-19 20:08:32 +0800

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    base: '/',
+    title: '博客',
+    description: 'Vuepress blog demo',
+
+    themeConfig: {
+        // 你的GitHub仓库，请正确填写
+        repo: 'https://github.com/lxr17',
+
+        // 自定义仓库链接文字。
+        repoLabel: 'GitHub',
+
+        // 导航栏
+        nav: [
+            {text: '首页', link: '/'},
+            {text: '第一篇博客', link: '/blog/FirstBlog.md'}
+        ],
+
+        // 侧边栏
+        sidebar: [
+            ['/blog/FirstBlog.md', '我的第一篇博客'],
+            ['/blog/Blog2.md', '我的第二篇博客']
+        ]
+    }
+}
